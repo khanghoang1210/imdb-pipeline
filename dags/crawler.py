0adc8ee3e0cf2d3242c48afdc7e5a696754b3d16
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2023-07-29 22:32:24 +0700

    add function crawl data imdb site

--- dags/crawler.py
+++ dags/crawler.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
-from datetime import datetime, date
+from datetime import date
 import requests
 
 #from src.utils import get_variables as gav
 
-def exrtract_id_movie(url):
+def exrtract_id_movie(url: str):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     div = soup.find("div",{"class":"a-box-inner"})
@@ -13,7 +13,7 @@ def exrtract_id_movie(url):
 
     return id
                       
-def crawl_box_office(date:date):
+def crawl_box_office(date: date):
     # url_prefix = gav.box_url
 
     # url = f"{url_prefix}{date}/"
@@ -27,28 +27,55 @@ def crawl_box_office(date:date):
     fact_movie = []
 
     for row in rows[1:]:
-        movie_info = {}
-        movie_info['rank'] = row.find("td",{"class":"mojo-header-column"}).text
+        movie_daily_info = {}
+        movie_daily_info['rank'] = row.find("td",{"class":"mojo-header-column"}).text
 
         revenue = row.find("td",{"class":"mojo-field-type-money"}).text
-        movie_info['revenue'] = revenue[1:]
+        movie_daily_info['revenue'] = revenue[1:]
 
-        movie_info['partition_date'] = date.strftime("%d-%m-%Y")
+        movie_daily_info['partition_date'] = date.strftime("%d-%m-%Y")
 
         href = row.find("td",{"class": "mojo-field-type-release"}).find("a").attrs['href']
         url_detail = url[:29] + href
-        movie_info['id'] = exrtract_id_movie(url_detail)
+        movie_daily_info['id'] = exrtract_id_movie(url_detail)
 
-        fact_movie.append(movie_info)
+        fact_movie.append(movie_daily_info)
 
     print(fact_movie)
     print(len(fact_movie))
     return fact_movie
 
 
+def crawl_imdb():
+    dim_movie = []
+    url = 'https://www.imdb.com/title/tt7599146/'
+    headers = {'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    title = soup.find("span", {"class":"fDTGTb"}).text
+    movie_id = url.split('/')[4]
+    url = url
+    director = soup.find("a", {"class": "ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link"}).text
+    dim_movie.append(movie_id)
+    dim_movie.append(title)
+    dim_movie.append(director)
+    dim_movie.append(url)
+    print(dim_movie)
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
-    crawl_box_office(date=date(2023, 7, 27))
+    #crawl_box_office(date=date(2023, 7, 27))
+    crawl_imdb()
 
 
         
