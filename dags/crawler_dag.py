0762c8faf1ebf6976cb631067af9d6d4bc050c2d
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2023-08-02 14:02:29 +0700

    schedule crawler daily

--- dags/crawler_dag.py
+++ dags/crawler_dag.py
@@ -6,12 +6,13 @@ from airflow import DAG
 from crawler import crawl_box_office, crawl_imdb
 
     
-start_date = date(2023, 7, 27)
-id_list = []
-box_office_item = crawl_box_office(start_date)
+# start_date = datetime(2023, 7, 27)
+# end_date = (datetime.now() - timedelta(days=2)).replace(hour=0, minute=0, second=0, microsecond=0)
 
-for item in box_office_item:
-    id_list.append(item['id'])
+# Hàm để chuyển đổi định dạng ngày sang 'dd-mm-yyyy'
+def format_date_to_ddmmyyyy(ds):
+    return datetime.strptime(ds, '%Y-%m-%d').strftime('%d-%m-%Y')
+
 
 default_args = {
     'owner' : 'khanghoang',
@@ -21,24 +22,39 @@ default_args = {
 
 with DAG (
     default_args=default_args,
-    dag_id='crawl_fact',
-    description='crawler fact data from box office site',
-    start_date=datetime(2023, 7, 26),
-    schedule_interval='0 0 * * 4'
+    dag_id='crawl_data_v1',
+    description='crawler data from box office and imdb',
+    start_date=datetime(2023, 7, 25),
+    #end_date=datetime(2023, 7, 31),
+    schedule_interval='@daily',
+    catchup=False,
 ) as dag:
-    
+    # exc_date = '{{ ds }}'
+    # exc_date_formatted = exc_date.date()
 
     task1 = PythonOperator(
-        task_id = 'crawl_fact_data',
+        task_id = f'crawl_fact_data',
         python_callable=crawl_box_office,
-        op_kwargs={'date': start_date}
+        op_kwargs={'date': "{{ ti.xcom_pull(task_ids='format_date')['formatted_date'] }}"}
     )
 
-    task1
 
-    # task2 = PythonOperator(
-    #     task_id = 'crawl_dim_data',
-    #     python_callable=crawl_imdb,
-    #     provide_context = True
-    # )
-    # task2
+    task2 = PythonOperator(
+        task_id = f'crawl_dim_data',
+        python_callable=crawl_imdb,
+        op_kwargs={'date': "{{ ti.xcom_pull(task_ids='format_date')['formatted_date'] }}"}
+    )
+
+    format_date_task = PythonOperator(
+    task_id='format_date',
+    python_callable=format_date_to_ddmmyyyy,
+    provide_context=True,
+    op_args=["{{ ds }}"]
+)
+
+    format_date_task >> task1
+    format_date_task >> task2
+
+    
+
+        
