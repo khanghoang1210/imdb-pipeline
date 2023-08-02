166aec6e3e3f8b11778e3faa0137cd98b4148cce
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2023-08-02 12:01:58 +0700

    add function get all variables

---
+++ dags/get_all_variables.py
@@ -0,0 +1,6 @@
+# Get User-Agent
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+
+# Get path to crawl
+box_office_path = "https://www.boxofficemojo.com/date/"
+imdb_path = "https://www.imdb.com/title/"
