5bdc4b12d04308ef4c983069c94676805a272e01
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2023-08-09 16:08:14 +0700

    insert dim data to postgres staging

--- dags/crawler_dag.py
+++ dags/crawler_dag.py
@@ -7,23 +7,41 @@ from crawler import crawl_box_office, crawl_imdb
 import json
 
 
-def read_and_insert_data_to_postgres(**kwargs):
+def read_and_insert_fact_data(**kwargs):
     ti = kwargs['ti']
 
     crawled_data = ti.xcom_pull(task_ids='crawl_fact_data')
     data = f"""{crawled_data}"""
     data_clean = data.replace("'", '"')
-    json_data = json.loads(data_clean)
+    json_fact_data = json.loads(data_clean)
 
     pg_hook = PostgresHook(postgres_conn_id='postgres_localhost')
 
-    for item in json_data:
+    for item in json_fact_data:
         sql = """
-        INSERT INTO test_fact (rank, revenue, partition_date, id)
+        INSERT INTO movie_revenue (rank, revenue, partition_date, id)
         VALUES (%s, %s, %s, %s)
         """
         pg_hook.run(sql, parameters=(item['rank'], item['revenue'], item['partition_date'], item['id']))
 
+
+
+def read_and_insert_dim_data(**kwargs):
+    ti = kwargs['ti']
+    crawled_data = ti.xcom_pull(task_ids='crawl_dim_data')
+    
+    data = f"""{crawled_data}"""
+    data_clean = data.replace("'", '"')
+    json_dim_data = json.loads(data_clean)
+
+    pg_hook = PostgresHook(postgres_conn_id='postgres_localhost')
+    
+    for item in json_dim_data:
+        sql = """
+            insert into movies (title, movie_id, url, director)
+            values (%s, %s, %s, %s)
+            """
+        pg_hook.run(sql, parameters=(item['title'], item['movie_id'], item['url'], item['director']))
 
 default_args = {
     'owner' : 'khanghoang',
@@ -51,23 +69,20 @@ with DAG (
     )
 
 
-    # crawl_dim_data = PythonOperator(
-    #     task_id = 'crawl_dim_data',
-    #     python_callable=crawl_imdb,
-    #     op_kwargs={'date': '{{ ds }}'}
-    # )
+    crawl_dim_data = PythonOperator(
+        task_id = 'crawl_dim_data',
+        python_callable=crawl_imdb,
+        op_kwargs={'date': '{{ ds }}'},
+        provide_context = True,
+        do_xcom_push=True
+    )
 
-    # save_data = PythonOperator(
-    #     task_id='save',
-    #     python_callable=save_fact_data,
-    #     op_kwargs={'data': data}
-    # )
-    # save_data
-    create = PostgresOperator(
-        task_id='create',
+
+    create_fact_table = PostgresOperator(
+        task_id='create_fact_table',
         postgres_conn_id='postgres_localhost',
         sql="""
-            CREATE TABLE IF NOT EXISTS test_fact (
+            CREATE TABLE IF NOT EXISTS movie_revenue (
             rank integer,
             revenue text,
             partition_date text,
@@ -76,15 +91,36 @@ with DAG (
         )
         """
     )
+
+    create_dim_table = PostgresOperator(
+        task_id='create_dim_table',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            CREATE TABLE IF NOT EXISTS movies (
+            title text,
+            movie_id text,
+            url text,
+            director text,
+            primary key(movie_id)
+        )
+        """
+    )
     
-    insert_data_to_postgres = PythonOperator(
-    task_id='insert_data_to_postgres',
-    python_callable=read_and_insert_data_to_postgres,
+    insert_fact_data_to_postgres = PythonOperator(
+    task_id='insert_fact_data_to_postgres',
+    python_callable=read_and_insert_fact_data,
     provide_context=True,
     op_kwargs={} 
     )
-    
-    crawl_fact_data>>create >> insert_data_to_postgres
 
+    insert_dim_data_to_postgres = PythonOperator(
+        task_id = 'insert_dim_data_to_postgres',
+        python_callable=read_and_insert_dim_data,
+        provide_context = True,
+        op_kwargs={}
+    )
+    
+    crawl_fact_data >> create_fact_table >> insert_fact_data_to_postgres
+    crawl_dim_data >> create_dim_table >> insert_dim_data_to_postgres
 
         
