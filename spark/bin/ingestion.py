08f1dadbedcd6e8e5ad25680015af2d5c69370ab
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2023-08-14 22:07:23 +0700

    add function incremental loading

--- spark/bin/ingestion.py
+++ spark/bin/ingestion.py
@@ -1,4 +1,5 @@
 # Import libraries
+from datetime import datetime
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit, monotonically_increasing_id, to_date
 import logging
@@ -39,6 +40,9 @@ try:
     # Import necessary Hadoop classes
     java_import(spark._jvm, "org.apache.hadoop.fs.FileSystem")
     java_import(spark._jvm, "org.apache.hadoop.fs.Path")
+    # jvm = spark_session._jvm
+    # jsc = spark_session._jsc
+    # fs = jvm.org.apache.hadoop.fs.FileSystem.get(jsc.hadoopConfiguration())
 
     hdfs_client = InsecureClient("http://localhost:9870", user="hive")
 
@@ -49,39 +53,37 @@ try:
     file_path = f"hdfs://localhost:9000/hive/user/datalake/{table_name}"
 
     # Path to datalake   
-    table_location = f"/hive/user/datalake/"
+    table_location = f"/hive/user/datalake/{table_name}"
 
     query = ""
-    last_crawled_date = df.select("crawled_date").agg({"crawled_date": "max"}).collect()[0]["max(crawled_date)"]
+    
     if not hdfs_client.status(table_location, strict=False):  
-        hdfs_client.makedirs(table_location)
-        # df = spark.read.csv(file_path)
-        # record_id = df
-        logger.info(f"Path '{table_location}' created successfully in HDFS.")
+        #hdfs_client.makedirs(table_location)
+        query = f"(SELECT * FROM {table_name}) AS tmp"
+        logger.info(f"Path '{table_location}' created successfully in HDFS with new data")
     else:
-        logger.info(f"Path '{table_location}' already exists in HDFS.")
-
-
+        df = spark.read.csv(file_path, header=True)
+        crawled_date_str = str(df.select("crawled_date").agg({"crawled_date": "max"}).collect()[0]["max(crawled_date)"])
+        last_crawled_date = datetime.strptime(crawled_date_str, '%Y-%m-%d').date()
+        query = f"(SELECT * FROM {table_name} WHERE (crawled_date > DATE '{last_crawled_date}')) AS tmp"
+        logger.info(f"Path '{table_location}' already exists in HDFS. Incremental load is completed.")
 
     # Read data from Postgres to Spark DataFrame
     jdbcDF = spark.read \
               .format("jdbc") \
               .option("url", "jdbc:postgresql://localhost:5434/postgres") \
-              .option("dbtable", table_name) \
+              .option("dbtable", query) \
               .option("user", user) \
               .option("password", password) \
               .option("driver", "org.postgresql.Driver") \
               .load()\
               
     logger.info("Read data completed.")
-    jdbcDF = jdbcDF.withColumn("idx", monotonically_increasing_id())
     
     # Validate dataframe
     
     jdbcDF.show()
     jdbcDF.printSchema()
-    
-
     
     # Load spark dataframe into datalake
     logger.info("Load Spark DataFrame into HDFS is started...")
@@ -89,7 +91,7 @@ try:
     jdbcDF.coalesce(1) \
         .write \
         .format('csv') \
-        .save(file_path, header=True, compression='snappy')
+        .save(file_path, header=True, compression='snappy', mode='append')
 
     logger.info("Ingestion is completed.")
  
