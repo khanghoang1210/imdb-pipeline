341545baf4bb437e970278527bfc8ae2ea15db8a
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2023-08-24 17:50:44 +0700

    get rank change

--- spark/bin/transformation.py
+++ spark/bin/transformation.py
@@ -1,6 +1,8 @@
 # Import libraries
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+from pyspark.sql.window import Window
+from datetime import timedelta, datetime
 ## Report:
     # 1.ID
     # 2.Title
@@ -23,28 +25,48 @@ spark = SparkSession\
 df_movies = spark.read.csv("hdfs://localhost:9000/hive/user/datalake/movies", header=True).drop("year","month","day")
 df_revenue = spark.read.csv("hdfs://localhost:9000/hive/user/datalake/movie_revenue", header=True).drop("year","month","day")
 
-# Transform column revenue
-df_revenue = df_revenue.withColumn("revenue",regexp_replace(col('revenue'), ',', '').cast("int"))
 
-# Get available columns
-df = df_movies.select(col("movie_id").alias("id"), col("title"))
-total_revenue = df_revenue.groupBy("id").agg(sum("revenue").alias("total_revenue"))
+# # Transform datatype of columns
+df_revenue = df_revenue.withColumn("rank", df_revenue.rank.cast("int"))
+df_revenue = df_revenue.withColumn("crawled_date", df_revenue.crawled_date.cast("date"))
+# df_revenue = df_revenue.withColumn("revenue",regexp_replace(col('revenue'), ',', '').cast("int"))
 
-# Join main columns 
-df_joined = df_revenue.join(df_movies, df_revenue.id == df_movies.movie_id).drop(df_movies.crawled_date)
+# # Get available columns
+# df = df_movies.select(col("movie_id").alias("id"), col("title"))
+# total_revenue = df_revenue.groupBy("id").agg(sum("revenue").alias("total_revenue"))
 
-# Get latest week
-max_crawled_date = df_joined.select(max(col('crawled_date'))).collect()[0][0]
-seven_days_ago = max_crawled_date - expr("INTERVAL 7 DAYS")
-df_filtered = df_joined.filter(col('crawled_date') >= seven_days_ago)
+# # Join main columns 
+# df_joined = df_revenue.join(df_movies, df_revenue.id == df_movies.movie_id).drop(df_movies.crawled_date)
 
-# Get week revenue of each movie
-df_week_revenue = df_filtered.groupBy('movie_id').agg(sum('revenue').alias('week_revenue'))
+# # Get latest week
+# max_crawled_date = df_joined.select(max(col('crawled_date'))).collect()[0][0]
+# seven_days_ago = max_crawled_date - expr("INTERVAL 7 DAYS")
+# df_filtered = df_joined.filter(col('crawled_date') >= seven_days_ago)
 
-# Aggarate dataframe
-df_res = df.join(total_revenue, df["id"]==total_revenue["id"],"right").join(df_week_revenue, df.id==df_week_revenue.movie_id).drop("movie_id")\
-                                                                                                                            .drop(total_revenue["id"])
+# # Get week revenue of each movie
+# df_week_revenue = df_filtered.groupBy('movie_id').agg(sum('revenue').alias('week_revenue'))
 
+# # Join dataframe
+# df_res = df.join(total_revenue, df["id"]==total_revenue["id"],"right")\
+#            .join(df_week_revenue, df.id==df_week_revenue.movie_id).drop("movie_id")\
+#                                                                   .drop(total_revenue["id"])
+
+
+
+# df_res.show()
+# df_filtered.show(300)
+
+window_spec = Window.orderBy(col("crawled_date")).partitionBy("id")
+df = df_revenue.withColumn("previous_date", lag("crawled_date").over(window_spec))
+
+df_rank = df.groupBy("id").agg(max("crawled_date").alias("max_date"))
+# df.select(coalesce(col("previous_date")))
+df.show()
+df_rank.show(100)
+# df_join = df.join(df_rank).where(df_rank['max_date']==df['previous_date']) & (df_rank['id']==df['id'])
+# df_join.show()
+# df_revenue.printSchema()
+#df_revenue = df_revenue.withColumn('crawled_date', col('crawled_date').cast('date'))
 
 
 
